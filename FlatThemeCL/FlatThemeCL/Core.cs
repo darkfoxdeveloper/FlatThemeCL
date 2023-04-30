5a6a3b69e3111cb7208ccd68fbc8e7defa5c3d46
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2023-04-30 14:57:57 +0200

    Basic changes on controls for customize

---
+++ FlatThemeCL/FlatThemeCL/Core.cs
@@ -0,0 +1,41 @@
+﻿using CLCore;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FlatThemeCL.Plugins
+{
+    public class FlatThemeCL : IPlugin
+    {
+        public string Explanation
+        {
+            get
+            {
+                return "This plugin is a example for use a Custom Theme in Conquer Loader.";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "FlatThemeCL";
+            }
+        }
+
+        public PluginType PluginType { get; } = PluginType.FREE;
+
+        public void Init()
+        {
+            Control btnStart = CLTheme.MainControls.Find("btnStart", false).FirstOrDefault();
+            Control btnSettings = CLTheme.MainControls.Find("btnSettings", false).FirstOrDefault();
+            btnStart.Text = "PLAY NOW";
+            btnSettings.Text = "CONFIG";
+        }
+
+        public void Configure()
+        {
+            MessageBox.Show($"No need configuration for now.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
